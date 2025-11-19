e7059f8e75b64e38bbc6a165fa89a296987b9f33
diimas666 <uu36548@gmail.com>
2025-11-19 20:11:54 +0200

    add hamburger menu and adaptive on 768px

--- js/index.js
+++ js/index.js
@@ -43,4 +43,28 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   type();
+
+  //печатная машинка  офф
+
+  // бургер меню мобильное
+
+  const burger = document.querySelector(".header__hamburger");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const overlay = document.querySelector(".mobile-menu-overlay");
+  const closeBtn = document.querySelector(".mobile-menu__close");
+
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+    overlay.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+
+  overlay.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
 });
