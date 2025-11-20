64e9624f7a07d893591d5b9cdbbec48f7be53958
diimas666 <uu36548@gmail.com>
2025-11-20 18:45:21 +0200

    about section desktop finihed

--- js/index.js
+++ js/index.js
@@ -67,4 +67,77 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileMenu.classList.remove("active");
     overlay.classList.remove("active");
   });
+
+  // Находим секцию с цифрами
+  const aboutSection = document.querySelector(".about");
+  const counters = document.querySelectorAll(".counter");
+
+  // Функция анимации чисел
+  const startCounter = (counter) => {
+    const target = +counter.getAttribute("data-target"); // Конечное число
+    const duration = 8000; // Длительность анимации в мс (2 секунды)
+    const step = target / (duration / 16); // Вычисляем шаг (16мс - это 60fps)
+
+    let current = 0;
+
+    const updateCounter = () => {
+      current += step;
+
+      if (current < target) {
+        // Округляем в большую сторону и обновляем текст
+        counter.innerText = Math.ceil(current);
+        requestAnimationFrame(updateCounter);
+      } else {
+        // В конце ставим точное значение, чтобы не было дробей
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  };
+
+  // Настраиваем Observer (наблюдатель)
+  const observerOptions = {
+    root: null, // следим относительно всего окна
+    threshold: 0.3, // Анимация начнется, когда 30% секции будет видно
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      // Если секция появилась на экране
+      if (entry.isIntersecting) {
+        counters.forEach((counter) => {
+          startCounter(counter);
+        });
+        // Отключаем наблюдение, чтобы анимация сработала только один раз
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  // Запускаем слежение за секцией about
+  if (aboutSection) {
+    observer.observe(aboutSection);
+  }
+
+  // слайдер старт !!
+  // Ініціалізація слайдера відгуків
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Базові налаштування
+    slidesPerView: 1, // Показувати 1 слайд
+    spaceBetween: 30, // Відстань між слайдами
+    loop: true, // Нескінченний цикл
+    grabCursor: true, // Змінює курсор при наведенні
+    autoHeight: true, // Підлаштовувати висоту під контент слайда
+
+    // Навігація стрілками
+    navigation: {
+      nextEl: ".reviews-next",
+      prevEl: ".reviews-prev",
+    },
+    autoplay: {
+      delay: 5000, // Пауза 5 секунд
+      disableOnInteraction: false, // Не зупиняти після ручного перемикання
+    },
+  });
 });
